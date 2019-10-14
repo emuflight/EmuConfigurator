59c15705f40a682fada99acefe6b8de414a508cd
Tom Hensel <github@jitter.eu>
2019-10-15 01:54:31 +0200

    still piping

--- .travis.sh
+++ .travis.sh
@@ -3,16 +3,13 @@
 # travis continuous integration build script for
 # EmuConfigurator
 
-export PATH=$HOME/.local/bin:$PATH # "user" installs (pip and stuff)
-
 # get version string from 'version.h'
 export CFG_VERSION="$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')"
 
 # compose string to reference the artifacts (binaries)
 export PACKAGE_VERSION="${CFG_VERSION}-${TRAVIS_BUILD_NUMBER}"
 
-yarn test || exit $?
-yarn gulp apps
+yarn gulp apps || exit $?
 #if [ "$TRAVIS_OS_NAME" == "linux" ]; then yarn gulp release --chromeos; fi
 
 # process template for pushing to bintray
