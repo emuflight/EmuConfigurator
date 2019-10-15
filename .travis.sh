b5ac8006c65357f681b424a5f0ac7563ce57fab3
Tom Hensel <github@jitter.eu>
2019-10-15 05:56:17 +0200

    not polish but i do the polishing [skip travis]

--- .travis.sh
+++ .travis.sh
@@ -9,15 +9,13 @@ export CONFIGURATOR_VERSION="$(cat package.json | grep version | head -1 | awk -
 # compose string to reference the package
 export PACKAGE_VERSION="${CONFIGURATOR_VERSION}-${TRAVIS_BUILD_NUMBER}-${TRAVIS_OS_NAME}"
 
-# process template for pushing to bintray (deploy step will pick it up)
-j2 bintray-template.j2 -o bintray-conf.json
-#cat bintray-conf.json # DEBUG
-
 # build releases
 yarn install || exit $?
 yarn gulp release
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then yarn gulp release --chromeos; fi
 
-ls -lsa release/ # DEBUG
+# process template for pushing to bintray (deploy step will pick it up)
+j2 bintray-template.j2 -o bintray-conf.json
+#cat bintray-conf.json # DEBUG
 
 exit 0
