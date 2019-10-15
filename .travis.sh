faf1c5ea719cda9a0321699b8c4ff5450a4459cd
Tom Hensel <github@jitter.eu>
2019-10-15 02:45:17 +0200

    fix PATH on windows; do not package individual files but compress a zip

--- .travis.sh
+++ .travis.sh
@@ -7,11 +7,26 @@
 export CFG_VERSION="$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')"
 
 # compose string to reference the artifacts (binaries)
-export PACKAGE_VERSION="${CFG_VERSION}-${TRAVIS_BUILD_NUMBER}"
+export PACKAGE_VERSION="${CFG_VERSION}-${TRAVIS_BUILD_NUMBER}-${TRAVIS_OS_NAME}"
 
-yarn gulp apps || exit $?
-#if [ "$TRAVIS_OS_NAME" == "linux" ]; then yarn gulp release --chromeos; fi
+# compose name of zip file to create
+export ZIP_FILE="apps/EmuConfigurator-${$PACKAGE_VERSION}.zip"
 
 # process template for pushing to bintray
 j2 bintray-template.j2 -o bintray-conf.json
 cat bintray-conf.json # DEBUG
+
+yarn gulp apps
+#if [ "$TRAVIS_OS_NAME" == "linux" ]; then yarn gulp release --chromeos; fi
+
+case "${$TRAVIS_OS_NAME}" in
+    osx)
+        zip -qr ${ZIP_FILE} apps/emuflight-configurator/osx64
+        ;;
+    linux)
+        zip -qr ${ZIP_FILE} apps/emuflight-configurator/linux64
+        ;;
+    windows)
+        7z -r ${ZIP_FILE} apps/emuflight-configurator/win32
+        ;;
+esac
