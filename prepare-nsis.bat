cf803f3c658aa47c464e877d91c91d41c31f1b83
Tom Hensel <github@jitter.eu>
2019-10-16 20:19:19 +0200

    now that we have learned how to run MS-DOS BATCH FILES on travis we can try do to this right

--- prepare-nsis.bat
+++ prepare-nsis.bat
@@ -2,9 +2,9 @@
 
 setlocal
 
-powershell -Command Set-MpPreference -DisableArchiveScanning 'Set-ExecutionPolicy -ExecutionPolicy RemoteSigned'
-powershell -Command Set-MpPreference -DisableArchiveScanning 'get-host'
+powershell -Command 'Set-ExecutionPolicy -ExecutionPolicy RemoteSigned'
+powershell -Command 'get-host'
 
-iwr -useb get.scoop.sh | iex
-scoop bucket add nsis https://github.com/NSIS-Dev/scoop-nsis
-scoop install nsis/nsis
+powershell -Command 'iwr -useb get.scoop.sh | iex'
+powershell -Command 'scoop bucket add nsis https://github.com/NSIS-Dev/scoop-nsis'
+powershell -Command 'scoop install nsis/nsis'
