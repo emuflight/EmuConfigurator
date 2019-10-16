705c31538c14315a92f5a0c7720c035966a48e53
Tom Hensel <github@jitter.eu>
2019-10-16 19:15:10 +0200

    @AndreySemjonov explained how "nsis" should be invoked. let's try!

--- .travis.sh
+++ .travis.sh
@@ -9,12 +9,33 @@ export CONFIGURATOR_VERSION="$(cat package.json | grep version | head -1 | awk -
 # compose string to reference the package
 export PACKAGE_VERSION="${CONFIGURATOR_VERSION}-${TRAVIS_BUILD_NUMBER}-${TRAVIS_OS_NAME}"
 
-# build releases
+# install dependencies
 yarn install || exit $?
-yarn gulp release
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then yarn gulp release --chromeos; fi
 
-# process template for pushing to bintray (deploy step will pick it up)
+# build releases for each platform
+case "${TRAVIS_OS_NAME}" in
+	linux)
+		yarn gulp release --chromeos
+		break
+		;;
+	osx)
+		yarn gulp release --osx64
+		break
+		;;
+	windows)
+		iwr -useb get.scoop.sh | iex
+		scoop bucket add nsis https://github.com/NSIS-Dev/scoop-nsis
+		scoop install nsis/nsis
+		yarn gulp release --win32
+		break
+		;;
+	*)
+		echo "platform ${TRAVIS_OS_NAME} not supported for now."
+		exit 10
+		;;
+esac
+
+# process template for pushing to bintray ('deploy' step on travis will pick it up)
 j2 bintray-template.j2 -o bintray-conf.json
 #cat bintray-conf.json # DEBUG
 
