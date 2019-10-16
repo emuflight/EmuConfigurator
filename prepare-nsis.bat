917903126263ac6322225052ee142799137cd2fe
Tom Hensel <github@jitter.eu>
2019-10-16 19:58:08 +0200

    i'm not so good with powershell :)

--- prepare-nsis.bat
+++ prepare-nsis.bat
@@ -2,8 +2,8 @@
 
 setlocal
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-get-host
+powershell -Command Set-MpPreference -DisableArchiveScanning 'Set-ExecutionPolicy -ExecutionPolicy RemoteSigned'
+powershell -Command Set-MpPreference -DisableArchiveScanning 'get-host'
 
 iwr -useb get.scoop.sh | iex
 scoop bucket add nsis https://github.com/NSIS-Dev/scoop-nsis
