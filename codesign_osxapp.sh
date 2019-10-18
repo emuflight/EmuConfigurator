be3e94bb1a6bd90e69d42547fbaf7e2bc4175c5d
Tom Hensel <github@jitter.eu>
2019-10-18 04:01:22 +0200

    after lots of local fiddling.. lets check

--- codesign_osxapp.sh
+++ codesign_osxapp.sh
@@ -1,20 +1,19 @@
-#!/bin/sh -x
+#!/bin/sh
 #
 # The Emuflight Project
 
-APP_IDENTITY="Mac Developer: Tom Hensel (BC7GB98TFH)"
+#set -e
+
+APP_IDENTITY="BC7GB98TFH"
 CERTIFICATE_P12="sign/EmuCert.p12"
 KEYCHAIN="build.keychain"
 ENTITLEMENTS_CHILD="sign/entitlements-child.plist"
 ENTITLEMENTS_PARENT="sign/entitlements-parent.plist"
 
-APP_PATH="${HOME}/build/emuflight/EmuConfigurator/apps/emuflight-configurator/osx64/emuflight-configurator.app"
+APP_PATH="apps/emuflight-configurator/osx64/emuflight-configurator.app"
 ls -lsa "${APP_PATH}" || exit 2
 
 VERSION_NUMBER=`ls "${APP_PATH}/Contents/Versions/"`
-NWJS_FRAMEWORK="${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework"
-LIBNODE_DYLIB="libnode.dylib"
-LIBNODE_LINK_TO="Versions/A/${LIBNODE_DYLIB}"
 
 echo "${CERTIFICATE_OSX_P12}" | base64 — decode > $CERTIFICATE_P12
 
@@ -24,27 +23,34 @@ security unlock-keychain -p $KEYC_PASS $KEYCHAIN
 security import $CERTIFICATE_P12 -k $KEYCHAIN -P $CERT_PASS -T /usr/bin/codesign
 
 sign () {
-    OBJECT=$1
-    ENTITLEMENTS=$2
+    OBJECT="${1}"
+    ENTITLEMENTS="${2}"
 
-    codesign --force --verbose --verify --sign "$APP_IDENTITY" --entitlements "$ENTITLEMENTS" --deep "$OBJECT"
-    codesign --verify --deep --strict --verbose=2 "$OBJECT"
+    echo "signing: ${OBJECT}"
+    codesign --verbose=2 --force --verify --sign "${APP_IDENTITY}" --entitlements "${ENTITLEMENTS}" --deep "${OBJECT}"
+    echo "verifying: ${OBJECT}"
+    codesign --verbose=2 --verify --strict --deep "${OBJECT}"
 }
 
-sign "$APP/Contents/Versions/63.0.3239.84/nwjs Framework.framework/libnode.dylib" "$ENTITLEMENTS_CHILD"
-sign "$APP/Contents/Versions/63.0.3239.84/nwjs Framework.framework/Helpers/crashpad_handler" "$ENTITLEMENTS_CHILD"
-sign "$APP/Contents/Versions/63.0.3239.84/nwjs Framework.framework/XPCServices/AlertNotificationService.xpc" "$ENTITLEMENTS_CHILD"
-sign "$APP/Contents/Versions/63.0.3239.84/nwjs Framework.framework/nwjs Framework" "$ENTITLEMENTS_CHILD"
-# sign "$APP/Contents/Versions/63.0.3239.84/nwjs Framework.framework" "$ENTITLEMENTS_CHILD"
-sign "$APP/Contents/Versions/63.0.3239.84/nwjs Helper.app" "$ENTITLEMENTS_CHILD"
-sign "$APP/Contents/Versions/63.0.3239.84/libffmpeg.dylib" "$ENTITLEMENTS_CHILD"
-sign "$APP" "$ENTITLEMENTS_PARENT"
+sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework/libnode.dylib" "$ENTITLEMENTS_CHILD"
+sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework/Helpers/crashpad_handler" "$ENTITLEMENTS_CHILD"
+sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework/XPCServices/AlertNotificationService.xpc" "$ENTITLEMENTS_CHILD"
+sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework/Versions/Current/nwjs Framework" "$ENTITLEMENTS_CHILD"
+sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework/libffmpeg.dylib" "$ENTITLEMENTS_CHILD"
+sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework" "$ENTITLEMENTS_CHILD"
+sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Helper.app" "$ENTITLEMENTS_CHILD"
+sign "${APP_PATH}" "$ENTITLEMENTS_PARENT"
 
 echo "fixing nwjs framework unsealed content"
-pushd "$NWJS_FRAMEWORK"
-mv "$LIBNODE_DYLIB" "$LIBNODE_LINK_TO"
-ln -s "$LIBNODE_LINK_TO"
+
+NWJS_FRAMEWORK="${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework"
+LIBNODE_DYLIB="libnode.dylib"
+LIBNODE_LINK_TO="Versions/A/${LIBNODE_DYLIB}"
+
+pushd "${NWJS_FRAMEWORK}"
+mv "${LIBNODE_DYLIB}" "${LIBNODE_LINK_TO}"
+ln -s "${LIBNODE_LINK_TO}"
 popd
 
-# /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $BUNDLE_ID" "$APP_PATH/Contents/Info.plist"
+# /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $BUNDLE_ID" "${APP_PATH}/Contents/Info.plist"
 # /usr/libexec/PlistBuddy -c "Set :com.apple.security.application-groups:0 $TEAM_ID.$BUNDLE_ID" "$ENTITLEMENTS_PARENT"
