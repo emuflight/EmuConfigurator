ed790f034c3cd9a0c8a4bd4dd2b555c90c6436e6
Tom Hensel <github@jitter.eu>
2019-10-18 07:20:07 +0200

    synchronize with knowledge gained from reading the docs

--- codesign_osxapp.sh
+++ codesign_osxapp.sh
@@ -5,10 +5,6 @@
 #
 # variables and composition
 #
-if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
-  echo "not running on travis and/or osx. skipping this script to prevent messing up ye keychain!"
-  exit 0
-fi
 
 CERTIFICATE_P12="sign/EmuCert.p12"
 KEYCHAIN="build.keychain"
@@ -26,33 +22,70 @@ if [ ! -d "${APP_PATH}" ]; then
   exit 2
 fi
 
+if [ -z "${APP_IDENTITY}" ]; then
+  echo "required variable APP_IDENTITY not set"
+  exit 3
+fi
+
+if [ -z "${BUNDLE_ID}" ]; then
+  echo "required variable BUNDLE_ID not set"
+  exit 4
+fi
+
+if [ -z "${TEAM_ID}" ]; then
+  echo "required variable TEAM_ID not set"
+  exit 5
+fi
+
+if [ ! -d "${APP_PATH}" ]; then
+  echo "unable to find application at: ${APP_PATH}"
+  exit 6
+fi
+
 if [ ! -f "${CERTIFICATE_P12}" ]; then
   echo "unable to find certifacte at: ${CERTIFICATE_P12}"
-  exit 3
+  exit 7
 fi
  
 if [ ! -f "${ENTITLEMENTS_CHILD}" ]; then
   echo "unable to find entitlement at: ${ENTITLEMENTS_CHILD}"
-  exit 4
+  exit 8
 fi
 
 if [ ! -f "${ENTITLEMENTS_PARENT}" ]; then
   echo "unable to find entitlement at: ${ENTITLEMENTS_PARENT}"
-  exit 5
+  exit 9
 fi
 
 #
 # keychain
 #
-security create-keychain -p "${KEYC_PASS}" "${KEYCHAIN}"
-security default-keychain -s "${KEYCHAIN}"
-security unlock-keychain -p "${KEYC_PASS}" "${KEYCHAIN}"
-echo "import cert to keychain"
-security import "${CERTIFICATE_P12}" -k "${KEYCHAIN}" -P "${CERT_PASS}" -T /usr/bin/codesign || exit 3
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "${KEYC_PASS}" "${KEYCHAIN}"
+if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+  security create-keychain -p "${KEYC_PASS}" "${KEYCHAIN}"
+  security default-keychain -s "${KEYCHAIN}"
+  security unlock-keychain -p "${KEYC_PASS}" "${KEYCHAIN}"
+  echo "import cert to keychain"
+  security import "${CERTIFICATE_P12}" -k "${KEYCHAIN}" -P "${CERT_PASS}" -T /usr/bin/codesign || exit 3
+  security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "${KEYC_PASS}" "${KEYCHAIN}"
+else
+  echo "not running on travis and/or osx. skipping 'keychain' part"
+fi
 
 /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier ${BUNDLE_ID}" "${APP_PATH}/Contents/Info.plist"
 /usr/libexec/PlistBuddy -c "Set :com.apple.security.application-groups:0 ${TEAM_ID}.${BUNDLE_ID}" "$ENTITLEMENTS_PARENT"
+
+#
+# unsealed content
+#
+echo "fixing nwjs framework unsealed content"
+NWJS_FRAMEWORK="${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework"
+LIBNODE_DYLIB="libnode.dylib"
+LIBNODE_LINK_TO="Versions/A/${LIBNODE_DYLIB}"
+
+pushd "${NWJS_FRAMEWORK}"
+mv "${LIBNODE_DYLIB}" "${LIBNODE_LINK_TO}"
+ln -s "${LIBNODE_LINK_TO}"
+popd
 
 #
 # signing
@@ -74,23 +107,10 @@ sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework/V
 sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework/libffmpeg.dylib" "$ENTITLEMENTS_CHILD"
 sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework" "$ENTITLEMENTS_CHILD"
 sign "${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Helper.app" "$ENTITLEMENTS_CHILD"
-#sign "${APP_PATH}" "$ENTITLEMENTS_PARENT"
-
-#
-# unsealed content
-#
-echo "fixing nwjs framework unsealed content"
-NWJS_FRAMEWORK="${APP_PATH}/Contents/Versions/${VERSION_NUMBER}/nwjs Framework.framework"
-LIBNODE_DYLIB="libnode.dylib"
-LIBNODE_LINK_TO="Versions/A/${LIBNODE_DYLIB}"
-
-pushd "${NWJS_FRAMEWORK}"
-mv "${LIBNODE_DYLIB}" "${LIBNODE_LINK_TO}"
-ln -s "${LIBNODE_LINK_TO}"
-popd
+sign "${APP_PATH}" "$ENTITLEMENTS_PARENT"
 
 #
 # bundle
 #
-spctl --assess --type execute "${APP_PATH}"
-spctl --assess --verbose=4 "${APP_PATH}"
+spctl --assess --type execute "${APP_PATH}" || true
+spctl --assess --verbose=4 "${APP_PATH}" || true
