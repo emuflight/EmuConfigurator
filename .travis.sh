c8277666bf0982c47ce049da5d38cd784f854886
gretel <code@jitter.eu>
2020-03-13 18:41:46 +0100

    change order for readability

--- .travis.sh
+++ .travis.sh
@@ -3,11 +3,7 @@
 # travis continuous integration build script for
 # EmuConfigurator
 
-# get version string from 'package.json'
-export CONFIGURATOR_VERSION="$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')"
-
-# compose string to reference the package
-export PACKAGE_VERSION="${CONFIGURATOR_VERSION}-${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}-${TRAVIS_OS_NAME}"
+## build
 
 # install dependencies
 yarn --frozen-lockfile || exit $?
@@ -34,6 +30,14 @@ case "${TRAVIS_OS_NAME}" in
 esac
 
 ls -lsa release/
+
+## bintray
+
+# get version string from 'package.json'
+export CONFIGURATOR_VERSION="$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')"
+
+# compose string to reference the package
+export PACKAGE_VERSION="${CONFIGURATOR_VERSION}-${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}-${TRAVIS_OS_NAME}"
 
 # process template for pushing to bintray ('deploy' step on travis will pick it up)
 j2 bintray-template.j2 -o bintray-conf.json
