d901ea1ab02206c1f97c89e93634dac3af2df1c6
gretel <code@jitter.eu>
2020-03-14 00:04:44 +0100

    put build number first on bintray for sorting; stable version of 'dpl' does not glob per default so enable explicitly; formatting

--- .travis.sh
+++ .travis.sh
@@ -7,7 +7,7 @@
 export CONFIGURATOR_VERSION="$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')"
 
 # compose string to reference the package
-export PACKAGE_VERSION="${CONFIGURATOR_VERSION}-${TRAVIS_BUILD_NUMBER}-${TRAVIS_OS_NAME}"
+export PACKAGE_VERSION="${TRAVIS_BUILD_NUMBER}-${CONFIGURATOR_VERSION}-${TRAVIS_OS_NAME}"
 
 # install dependencies
 yarn install || exit $?
