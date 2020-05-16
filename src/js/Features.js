b9c863adb94c384298d6a13f08d915d751dc77c8
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2020-05-16 12:53:59 -0500

    quick and dirty hide removed features for 0.3.0

--- src/js/Features.js
+++ src/js/Features.js
@@ -83,8 +83,13 @@ var Features = function (config) {
         }
 
         if (semver.gte(CONFIG.apiVersion, "1.36.0")) {
+            //quick & dirty 0.3.0
+            if (semver.lt(CONFIG.flightControllerVersion, "0.3.0")) {
+                features.push(
+                    {bit: 28, group: 'other', name: 'ANTI_GRAVITY'}
+                );
+            }
             features.push(
-                {bit: 28, group: 'other', name: 'ANTI_GRAVITY'},
                 {bit: 29, group: 'other', name: 'DYNAMIC_FILTER'}
             );
         }
