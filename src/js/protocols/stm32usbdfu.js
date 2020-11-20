0237a8f1c853227326892d7dae6f14bdc26faa08
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2020-11-20 09:22:51 -0600

    set default timeout 1000ms rather than 0

--- src/js/protocols/stm32usbdfu.js
+++ src/js/protocols/stm32usbdfu.js
@@ -438,7 +438,7 @@ STM32DFU_protocol.prototype.controlTransfer = function (direction, request, valu
     // timeout support was added in chrome v43
     var timeout;
     if (typeof _timeout === "undefined") {
-	timeout = 5; // default is 0 (according to chrome.usb API)
+	timeout = 1000; // default is 0 (according to chrome.usb API) //1000ms is 1s
     } else {
         timeout = _timeout;
     }
